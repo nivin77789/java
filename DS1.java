5faa3bde913f56d429f0d5a84dd91c2a4bd01c62
nivin77789 <nivin77789@gmail.com>
2023-06-21 23:25:00 +0530

    new3

--- DS1.java
+++ DS1.java
@@ -1,11 +1,12 @@
 import javax.naming.spi.DirStateFactory.Result;
-
+import java.util.*;
 public class DS1{
     public static void main(String args[])
     {
+        Scanner sc=new Scanner(System.in);
+        String c=sc.next();
         int a=23;
         int b=77;
-        String c="Result = ";
         int result=a+b;
         System.out.println(c+""+result);
     }
