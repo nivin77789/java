135eb671737e902f9c7b03e35fe3c1cec6d29348
nivin77789 <nivin77789@gmail.com>
2023-06-21 22:36:21 +0530

    new

--- DS1.java
+++ DS1.java
@@ -1,6 +1,8 @@
 public class DS1{
     public static void main(String args[])
     {
-        System.out.print("Hello World");
+        byte a=23;
+        String c="Nivin";
+        System.out.println("Hi "+c+ " age : "+a);
     }
 }
