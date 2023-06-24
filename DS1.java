6237a5634368d9a8fac592031a0d712a14e8a683
nivin77789 <nivin77789@gmail.com>
2023-06-24 15:58:38 +0530

    l2

--- DS1.java
+++ DS1.java
@@ -3,12 +3,13 @@ import java.util.*;
 public class DS1{
     public static void main(String args[])
     {
+        //new
         Scanner sc=new Scanner(System.in);
         String c=sc.nextLine();
         System.out.println(c+"is your name");
         int a=sc.nextInt();
         int b=77;
         int result=a+b;
-        System.out.println(c+""+result);
+        System.out.println(c+"is your name and the result is : "+result);
     }
 }
