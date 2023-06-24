71e450a8fcb6cdf5d97b88f1ff534da263757bd8
nivin77789 <nivin77789@gmail.com>
2023-06-24 18:18:55 +0530

    avg

--- DS1.java
+++ DS1.java
@@ -3,13 +3,19 @@ import java.util.*;
 public class DS1{
     public static void main(String args[])
     {
-        //new
-        Scanner sc=new Scanner(System.in);
-        String c=sc.nextLine();
-        System.out.println(c+"is your name");
-        int a=sc.nextInt();
-        int b=77;
-        int result=a+b;
-        System.out.println(c+"is your name and the result is : "+result);
+        // Scanner sc=new Scanner(System.in);
+        // String c=sc.nextLine();
+        // System.out.println(c+"is your name");
+        // int a=sc.nextInt();
+        // int b=77;
+        // int result=a+b;
+        // System.out.println(c+"is your name and the result is : "+result);
+System.out.println("Enter 3 numbers");
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+int c=sc.nextInt();
+int avg= (a+b+c)/3;
+System.out.println("the average of two numbers is : "+avg);
     }
 }
