c0caa3777a7e1b40d640731a08dac6446aab6670
nivin77789 <nivin77789@gmail.com>
2023-06-25 14:37:55 +0530

    day4

--- DS1.java
+++ DS1.java
@@ -10,14 +10,25 @@ public class DS1{
         // int b=77;
         // int result=a+b;
         // System.out.println(c+"is your name and the result is : "+result);
-System.out.println("Enter the cost of pen , pencil and eraser");
+        //day3
+// System.out.println("Enter the cost of pen , pencil and eraser");
+// Scanner sc=new Scanner(System.in);
+// float a=sc.nextFloat();
+// float b=sc.nextFloat();
+// float c=sc.nextFloat();
+// float sum= a+b+c;
+// float total= ((sum/100)*18)+sum;
+// System.out.println("the total value of the products is : "+total);
+//day4
 Scanner sc=new Scanner(System.in);
-float a=sc.nextFloat();
-float b=sc.nextFloat();
-float c=sc.nextFloat();
-float sum= a+b+c;
-float total= ((sum/100)*18)+sum;
-System.out.println("the total value of the products is : "+total);
+int a=sc.nextInt();
+if (a>0) {
+    System.out.println("positive");
+} else if(a<0) {
+    System.out.println("negative");
+} else {
+    System.out.println("zero");
+}
 
     }
 }
