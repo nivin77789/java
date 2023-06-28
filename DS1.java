5ffab3115225b7d8a35512fd9be299cdfb277bc6
nivin77789 <nivin77789@gmail.com>
2023-06-28 20:15:21 +0530

    day6.1

--- DS1.java
+++ DS1.java
@@ -80,9 +80,9 @@ public class DS1{
 // System.out.println("Factorial: "+ fact);
 // System.out.println("Factorial is generated Successfully");
 
-for(int i=1; i<=5; i++)
+for(int i=5; i>=1; i--)
 {
-    for(int j=1; j<=i; j++)
+    for(int j=i; j>=1; j--)
     {
         System.out.print("*");
     }
