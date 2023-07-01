1ff5c0862a36c011f20c58afe4e2c4854cb8de8f
nivin77789 <nivin77789@gmail.com>
2023-07-01 19:38:57 +0530

    fact

--- DS1.java
+++ DS1.java
@@ -1,12 +1,20 @@
 import javax.naming.spi.DirStateFactory.Result;
 import java.util.*;
 public class DS1{
-    public static void hello(){
-        System.out.println("Hello World!");
+    public static int hello(int n){
+        int f=1;
+        for(int i=1; i<=n; i++)
+        {
+            f*=i;
+        }
+        return f;
     }
     public static void main(String args[])
     {
-hello();
-hello();
+        Scanner sc=new Scanner(System.in);
+System.out.println("Factorial number : ");
+        int n=sc.nextInt();
+int r=hello(n);
+System.out.println("Factorial is : "+r);
     }
 }
