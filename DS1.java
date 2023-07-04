8b345cba0d4dafd617fc7af9ee7068c957f7344a
nivin77789 <nivin77789@gmail.com>
2023-07-04 23:21:43 +0530

    odd or even

--- DS1.java
+++ DS1.java
@@ -1,17 +1,24 @@
 
 import java.util.*;
 public class DS1{
-    public static int hello(int a , int b , int c){
-    return ((a+b+c)/3);
+    public static Boolean hello(int a ){
+    if(a%2==0){
+        return true;
+    } else {
+        return false;
+    }
     }
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-System.out.println("enter three number : ");
+System.out.println("enter a number : ");
         int a=sc.nextInt();
-        int b=sc.nextInt();
-        int c=sc.nextInt();
+       if(hello(a)){
+System.out.println("the number is EVEN");
+       } else {
+        System.out.println("the number is ODD");
+       }
 
-System.out.println("the average of three number is "+hello(a,b,c));
+
     }
 }
