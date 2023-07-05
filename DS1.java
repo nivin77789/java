13efefc006b11e76a1d08482c6a9d513d661ca25
nivin77789 <nivin77789@gmail.com>
2023-07-05 12:24:49 +0530

    palendron

--- DS1.java
+++ DS1.java
@@ -1,21 +1,31 @@
 
 import java.util.*;
 public class DS1{
-    public static Boolean hello(int a ){
-    
+    public static void hello(int a ){
+        int pal=a;
+        int num=0;
+        int rev=0;
         
+    while(a>0){
+num=a%10;
+rev=rev*10+num;
+a=a/10;
+    }
+    if(pal==rev){
+System.out.println("The reverse number is "+rev+ " and its a PALENDROM");
+    } else{
+        System.out.println("The reverse number is "+rev+ " and its not a PALENDROM");
+    }
+
+
+
     }
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
 System.out.println("enter a number : ");
         int a=sc.nextInt();
-       if(hello(a)){
-System.out.println("the number is EVEN");
-       } else {
-        System.out.println("the number is ODD");
-       }
-
+       hello(a);
 
     }
 }
