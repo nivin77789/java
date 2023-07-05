3d272e7259da0decee3027fa44364a515639c475
nivin77789 <nivin77789@gmail.com>
2023-07-05 13:42:09 +0530

    rev half patt

--- DS1.java
+++ DS1.java
@@ -3,13 +3,11 @@ import java.util.*;
 public class DS1{
     public static void hello(int a ,int b){
         for(int i=1; i<=a; i++){
-            for(int j=1; j<=b; j++){
-                if(i==1 || i==a | j==1 | j==b){
+            for(int j=1; j<=a-i; j++){
+                    System.out.print(" ");
+            }
+            for(int k=1; k<=i; k++){
                     System.out.print("*");
-                }else{
-System.out.print(" ");
-                }
-                
             }
 
 System.out.println();
@@ -25,7 +23,7 @@ System.out.println();
 System.out.println("enter a number : ");
         int a=sc.nextInt();
             int b=sc.nextInt();
-       hello(a,b);1
+       hello(a,b);
 
     }
 }
