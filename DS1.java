97c6603edc64d404e5960089ae0989facf0bd62d
nivin77789 <nivin77789@gmail.com>
2023-07-05 12:32:36 +0530

    Math.min

--- DS1.java
+++ DS1.java
@@ -1,21 +1,10 @@
 
 import java.util.*;
 public class DS1{
-    public static void hello(int a ){
-        int pal=a;
-        int num=0;
-        int rev=0;
-        
-    while(a>0){
-num=a%10;
-rev=rev*10+num;
-a=a/10;
-    }
-    if(pal==rev){
-System.out.println("The reverse number is "+rev+ " and its a PALENDROM");
-    } else{
-        System.out.println("The reverse number is "+rev+ " and its not a PALENDROM");
-    }
+    public static void hello(int a , int b ){
+        int r=Math.min(a,b);
+        System.out.println(r);
+    
 
 
 
@@ -23,9 +12,10 @@ System.out.println("The reverse number is "+rev+ " and its a PALENDROM");
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-System.out.println("enter a number : ");
+System.out.println("enter 2 number : ");
         int a=sc.nextInt();
-       hello(a);
+        int b=sc.nextInt();
+       hello(a,b);
 
     }
 }
