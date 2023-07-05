11b39d1e69884fc3263c9225c13ad8686faca787
nivin77789 <nivin77789@gmail.com>
2023-07-05 12:45:02 +0530

    sum of a number

--- DS1.java
+++ DS1.java
@@ -2,17 +2,25 @@
 import java.util.*;
 public class DS1{
     public static void hello(int a ){
-        int r=Math.abs(a);
-        System.out.println(r);
-    
+        int pal=a;
+        int num=0;
+        int sum=0;
+        
+    while(a>0){
+num=a%10;
+sum+=num;
+a=a/10;
+    }
+   
 
+System.out.println("the sum of the number is : "+sum);
 
 
     }
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-System.out.println("enter 2 number : ");
+System.out.println("enter a number : ");
         int a=sc.nextInt();
        hello(a);
 
