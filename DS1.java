81eb80dcc9a1b020dd0213dbb100230472f7da9e
nivin77789 <nivin77789@gmail.com>
2023-07-05 14:02:50 +0530

    floyeds patt my

--- DS1.java
+++ DS1.java
@@ -1,18 +1,19 @@
 
 import java.util.*;
 public class DS1{
-    public static void hello(int a ,int b){
-        for(int i=1; i<=a; i++){
-            for(int j=1; j<=a-i; j++){
-                    System.out.print(" ");
-            }
-            for(int k=1; k<=i; k++){
-                    System.out.print("*");
-            }
+    public static void hello(int a){
+        int j=1;
+        int b=1;
+        for(int k=1; k<=a; k++){
+for(int i=1; i<=b; i++){
+            System.out.print(j++);
 
-System.out.println();
+
         }
-   
+        b++;
+   System.out.println();
+        }
+        
 
 
 
@@ -22,8 +23,7 @@ System.out.println();
         Scanner sc=new Scanner(System.in);
 System.out.println("enter a number : ");
         int a=sc.nextInt();
-            int b=sc.nextInt();
-       hello(a,b);
+       hello(a);
 
     }
 }
