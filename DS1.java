6d4a8247e06532864d44a8c7906221d0bd990c93
nivin77789 <nivin77789@gmail.com>
2023-07-05 12:41:18 +0530

    Math.abs

--- DS1.java
+++ DS1.java
@@ -1,8 +1,8 @@
 
 import java.util.*;
 public class DS1{
-    public static void hello(int a  ){
-        double r=Math.sqrt(a);
+    public static void hello(int a ){
+        int r=Math.abs(a);
         System.out.println(r);
     
 
@@ -14,7 +14,6 @@ public class DS1{
         Scanner sc=new Scanner(System.in);
 System.out.println("enter 2 number : ");
         int a=sc.nextInt();
-        // int b=sc.nextInt();
        hello(a);
 
     }
