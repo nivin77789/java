1f3899b924d868a6e2dac6a20db15ff697c42bd6
nivin77789 <nivin77789@gmail.com>
2023-07-05 12:36:43 +0530

    Math.sqrt

--- DS1.java
+++ DS1.java
@@ -1,8 +1,8 @@
 
 import java.util.*;
 public class DS1{
-    public static void hello(int a , int b ){
-        double r=Math.pow(a,b);
+    public static void hello(int a  ){
+        double r=Math.sqrt(a);
         System.out.println(r);
     
 
@@ -14,8 +14,8 @@ public class DS1{
         Scanner sc=new Scanner(System.in);
 System.out.println("enter 2 number : ");
         int a=sc.nextInt();
-        int b=sc.nextInt();
-       hello(a,b);
+        // int b=sc.nextInt();
+       hello(a);
 
     }
 }
