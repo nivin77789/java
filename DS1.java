02a19ffe37e40143a7297258bb29a40bf703ce4f
nivin77789 <nivin77789@gmail.com>
2023-07-05 20:10:28 +0530

    diamond

--- DS1.java
+++ DS1.java
@@ -3,24 +3,36 @@ import java.util.*;
 public class DS1{
     public static void hello(int a){
         
-       for(int i=1; i<=a; i++)
+       for(int i=0; i<=a; i++)
        {
           for(int j=1; j<=a-i; j++)
           {
 System.out.print(" ");
           }
-          for(int j=1; j<=a; j++)
+          for(int j=1; j<=i*2-1; j++)
           {
-               if(i==1 | j==1 | i==a | j==a){
+             
 System.out.print("*");
-               } else {
-                    System.out.print(" ");
-               }
+              
 
           }
 System.out.println();
        }
-        
+         for(int i=a; i>=0; i--)
+       {
+          for(int j=a-i; j>=1; j--)
+          {
+System.out.print(" ");
+          }
+          for(int j=i*2-1; j>=1; j--)
+          {
+             
+System.out.print("*");
+              
+
+          }
+System.out.println();
+       }
 
 
 
