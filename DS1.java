72ea65c8dbe919850d2080df89d93ca0e83ddf0b
nivin77789 <nivin77789@gmail.com>
2023-07-07 21:45:38 +0530

    array

--- DS1.java
+++ DS1.java
@@ -1,48 +1,21 @@
 
 import java.util.*;
 public class DS1{
-    public static void hello(int a){
-        
-       for(int i=0; i<=a; i++)
-       {
-          for(int j=1; j<=a-i; j++)
-          {
-System.out.print(" ");
-          }
-          for(int j=1; j<=i*2-1; j++)
-          {
-             
-System.out.print("*");
-              
-
-          }
-System.out.println();
+    public static void hello(int arr[]){
+      System.out.println("enter a number : ");
+for(int i=0; i<4; i++){
+         System.out.println(arr[i]);
        }
-         for(int i=a; i>=0; i--)
-       {
-          for(int j=a-i; j>=1; j--)
-          {
-System.out.print(" ");
-          }
-          for(int j=i*2-1; j>=1; j--)
-          {
-             
-System.out.print("*");
-              
-
-          }
-System.out.println();
-       }
-
-
-
     }
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-System.out.println("enter a number : ");
-        int a=sc.nextInt();
-       hello(a);
-
+int arr[]=new int[10];
+    
+       for(int i=0; i<4; i++){
+         arr[i]=sc.nextInt();
+       }
+hello(arr);
+ 
     }
 }
