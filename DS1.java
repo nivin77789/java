a93e0706f6a64b5782b803ad39f3ccf27d000d1d
nivin77789 <nivin77789@gmail.com>
2023-07-07 22:04:01 +0530

    leanear search

--- DS1.java
+++ DS1.java
@@ -1,11 +1,16 @@
 
 import java.util.*;
 public class DS1{
-    public static void hello(int arr[]){
-      System.out.println("enter a number : ");
-for(int i=0; i<4; i++){
-         System.out.println(arr[i]);
-       }
+    public static void hello(int arr[], int key)
+    {
+
+for(int i=0; i<4; i++)
+{
+   if(arr[i]==key){
+               System.out.println("Element found at index "+i);
+            
+   } 
+}  
     }
     public static void main(String args[])
     {
@@ -15,7 +20,9 @@ int arr[]=new int[10];
        for(int i=0; i<4; i++){
          arr[i]=sc.nextInt();
        }
-hello(arr);
+         System.out.println("enter a number : ");
+       int key=sc.nextInt();
+hello(arr ,key);
  
     }
 }
