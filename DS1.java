0b4f81ec10205e20d863f9d8bd6a10ccd305c71c
nivin77789 <nivin77789@gmail.com>
2023-07-08 10:11:59 +0530

    binary search

--- DS1.java
+++ DS1.java
@@ -1,28 +1,35 @@
 
 import java.util.*;
 public class DS1{
-    public static void hello(int arr[], int key)
+    public static int hello(int arr[], int key)
     {
 
-for(int i=0; i<4; i++)
-{
-   if(arr[i]==key){
-               System.out.println("Element found at index "+i);
-            
-   } 
-}  
+int s=0;
+int e=arr.length;
+while(s<=e){
+   int mid=(s+e)/2;
+   if(arr[mid]==key){
+      return mid;
+   }
+   if(arr[mid]<key){
+      s=mid+1;
+   } else{
+      e=mid-1;
+   }
+}
+return -1;
     }
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-int arr[]=new int[10];
-    
-       for(int i=0; i<4; i++){
-         arr[i]=sc.nextInt();
-       }
-         System.out.println("enter a number : ");
+int arr[]={6,8,9,12,13,14,15,23};
+         System.out.println("enter key element: ");
        int key=sc.nextInt();
-hello(arr ,key);
- 
+int r=hello(arr ,key);
+ if(arr[r]==key){
+    System.out.println("Key element found at index : "+(r+1));
+ } else {
+    System.out.println("Key element not found");
+ }
     }
 }
