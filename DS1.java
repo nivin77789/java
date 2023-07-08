7bff465045092b27b949a088b4b53433126835d8
nivin77789 <nivin77789@gmail.com>
2023-07-08 11:40:16 +0530

    subarray

--- DS1.java
+++ DS1.java
@@ -3,28 +3,32 @@ import java.util.*;
 public class DS1{
     public static void hello(int arr[])
     {
-      int newarr[]=new int[arr.length];
-      int len=7;
-for(int i=0; i<7; i++){
-            newarr[i]=arr[len];
-            len--;
+     int ts=0;
+     for(int i=0; i<arr.length; i++){
+      int start=i;
+      for(int j=0; j<arr.length; j++){
+         int end=j;
+         for(int k=start; k<=end; k++){
+            System.out.print(arr[k]+"  ");
          }
-           System.out.println("array after revercing: ");
-         for(int i=0; i<arr.length; i++){
-             System.out.println(newarr[i]);
-         }
+         ts++;
+         System.out.println();
+      }
+      System.out.println();
+     }
+     System.out.println("total : "+ts);
     }
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-int arr[]={6,8,9,12,13,14,15,23};
-         System.out.println("array before revercing: ");
-         for(int i=0; i<arr.length; i++){
-            for(int j=0; j<arr.length; j++){
-               System.out.print("("+arr[i]+","+arr[j]+")  ");
-            }
-            System.out.println();
-         }
+int arr[]={2,4,6,8,10};
+         // System.out.println("array before revercing: ");
+         // for(int i=0; i<arr.length; i++){
+         //    for(int j=0; j<arr.length; j++){
+         //       System.out.print("("+arr[i]+","+arr[j]+")  ");
+         //    }
+         //    System.out.println();
+         // }
        hello(arr);
 
     }
