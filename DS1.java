60d96c14db9882f893ef4303ac7138ed7659367d
nivin77789 <nivin77789@gmail.com>
2023-07-08 11:07:06 +0530

    pairs2

--- DS1.java
+++ DS1.java
@@ -21,11 +21,7 @@ int arr[]={6,8,9,12,13,14,15,23};
          System.out.println("array before revercing: ");
          for(int i=0; i<arr.length; i++){
             for(int j=0; j<arr.length; j++){
-               System.out.print("(");
-                  System.out.print(arr[i]);
-                  System.out.print(",");
-                     System.out.print(arr[j]);
-                     System.out.print(")   ");
+               System.out.print("("+arr[i]+","+arr[j]+")  ");
             }
             System.out.println();
          }
