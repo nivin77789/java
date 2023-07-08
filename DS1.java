195d346abe3536b5371078c6cfa89240356078c1
nivin77789 <nivin77789@gmail.com>
2023-07-08 19:47:33 +0530

    sum of sub arradsf

--- DS1.java
+++ DS1.java
@@ -28,13 +28,14 @@ public class DS1{
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-int arr[]={2,4,-2,6,1,10,9,-2};
+int arr[]={2,4,-2,6,1,1,9,-2};
          // System.out.println("array before revercing: ");
          // for(int i=0; i<arr.length; i++){
          //    for(int j=0; j<arr.length; j++){
          //       System.out.print("("+arr[i]+","+arr[j]+")  ");
          //    }
-         //    System.out.println();
+        System.out.print("sjdh");
+       
          // }
        hello(arr);
 
