0611b9a290f3408b5a8ec9a71c7a4c90e6c98d35
nivin77789 <nivin77789@gmail.com>
2023-07-08 10:28:47 +0530

    rev

--- DS1.java
+++ DS1.java
@@ -1,35 +1,28 @@
 
 import java.util.*;
 public class DS1{
-    public static int hello(int arr[], int key)
+    public static void hello(int arr[])
     {
-
-int s=0;
-int e=arr.length;
-while(s<=e){
-   int mid=(s+e)/2;
-   if(arr[mid]==key){
-      return mid;
-   }
-   if(arr[mid]<key){
-      s=mid+1;
-   } else{
-      e=mid-1;
-   }
-}
-return -1;
+      int newarr[]=new int[arr.length];
+      int len=7;
+for(int i=0; i<7; i++){
+            newarr[i]=arr[len];
+            len--;
+         }
+           System.out.println("array after revercing: ");
+         for(int i=0; i<arr.length; i++){
+             System.out.println(newarr[i]);
+         }
     }
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
 int arr[]={6,8,9,12,13,14,15,23};
-         System.out.println("enter key element: ");
-       int key=sc.nextInt();
-int r=hello(arr ,key);
- if(arr[r]==key){
-    System.out.println("Key element found at index : "+(r+1));
- } else {
-    System.out.println("Key element not found");
- }
+         System.out.println("array before revercing: ");
+         for(int i=0; i<arr.length; i++){
+             System.out.println(arr[i]);
+         }
+       hello(arr);
+
     }
 }
