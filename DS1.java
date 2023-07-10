ed9889d860a16d055e5fce422cd2a80e136395f4
nivin77789 <nivin77789@gmail.com>
2023-07-10 13:28:33 +0530

    fgh

--- DS1.java
+++ DS1.java
@@ -3,12 +3,31 @@ import java.util.*;
 public class DS1{
     public static int hello(int arr[])
     {
-    return 0;
+      int n=arr.length;
+      //left max
+      int leftmax[]=new int[n];
+      leftmax[0]=arr[0];
+      for(int i=1; i<n; i++){
+         leftmax[i]=Math.max(arr[i],leftmax[i-1]);
+      }
+      //right max
+       int rightmax[]=new int[n];
+      rightmax[n-1]=arr[n-1];
+      for(int i=n-2; i>=0; i--){
+         rightmax[i]=Math.max(arr[i] ,rightmax[i+1]);
+      }
+
+      //traped water
+      int trappedwater=0;
+      for(int i=0; i<n; i++){
+         int waterlevel=Math.max(rightmax[i],leftmax[i]);
+         trappedwater+=waterlevel-arr[i];
+      }
+    return trappedwater;
     }
     public static void main(String args[])
     {
-        Scanner sc=new Scanner(System.in);
-int arr[]={4,2,0,6,3,2,5};
+int arr[]={3, 0, 2, 0, 4};
          System.out.println(hello(arr));
 
     }
