48eff99f535cc5e413e0af74e904bcae66205127
nivin77789 <nivin77789@gmail.com>
2023-07-11 19:35:01 +0530

    insertion sort

--- DS1.java
+++ DS1.java
@@ -35,15 +35,38 @@ for(int i=0; i<n-1; i++){
     }
   }
 }
+   System.out.println("Bubble sort");
   for(int k=0; k<n; k++){
-    System.out.println(arr[k]);
+    System.out.print(arr[k]);
+     System.out.print("  ");
   }
+  System.out.println("");
 
+    }
+    public static void sel(int arr[]){
+      int n=arr.length;
+      for(int i=0; i<n-1; i++){
+        int minpos=i;
+        for(int j=i+1; j<n; j++){
+if( arr[minpos]< arr[j]){
+minpos=j;
+}
+        }
+        int temp=arr[minpos];
+        arr[minpos]=arr[i];
+        arr[i]=temp;
+      }
+      System.out.println("insertion sort");
+       for(int i=0; i<n; i++){
+System.out.print(arr[i]);
+     System.out.print("  ");
+       }
     }
     public static void main(String args[])
     {
-int arr[]={2,1,8,5,9};
+int arr[]={3,6,7,2,1,8,5,9,4};
         hello(arr);
+        sel(arr);
 
     }
 }
