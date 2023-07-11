6b6a9bb159155308a54a56f79c6208fe442258f1
nivin77789 <nivin77789@gmail.com>
2023-07-11 22:00:51 +0530

    selection,bubble and insertion  sort

--- DS1.java
+++ DS1.java
@@ -66,9 +66,9 @@ System.out.print(arr[i]);
       int n=arr.length;
     
       for(int i=1; i<n; i++){
-        int curr=i;
+        int curr=arr[i];
         int prev=i-1;
-        while(prev>=0 && arr[prev]> arr[curr]){
+        while(prev>=0 && arr[prev]> curr){
           arr[prev+1]= arr[prev];
           prev--;
         }
@@ -80,6 +80,7 @@ System.out.print(arr[i]);
 System.out.print(arr[i]);
      System.out.print("  ");
        }
+          System.out.println("  ");
     }
     public static void main(String args[])
     {
