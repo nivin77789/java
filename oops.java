fd91385bf404d68eb05a36718d86e449256ec0c1
nivin77789 <nivin77789@gmail.com>
2023-07-24 21:35:49 +0530

    oops

---
+++ oops.java
@@ -0,0 +1,19 @@
+public class oops {
+    public static void main(String args[]) {
+        pen p1=new pen();
+        p1.tip=99;
+        
+System.out.println(p1.color +" colour and the tip is "+ p1.tip);
+    }
+    
+}
+class pen{
+    String color="blue";
+    int tip=1;
+    void setColor(String newcolor){
+        color= newcolor;
+    }
+    void settip(int newtip){
+        tip= newtip;
+    }
+}
