029b60f33b5034abe94030fb1a477d99ee317557
nivin77789 <nivin77789@gmail.com>
2023-07-26 12:54:47 +0530

    abstraction

--- oops.java
+++ oops.java
@@ -1,17 +1,19 @@
+import java.util.*;
 public class oops {
     public static void main(String args[]) {
-        pen p1=new pen();
+        News p1=new News();
         p1.tip=99;
         
 System.out.println(p1.color +" colour and the tip is "+ p1.tip);
 p1.setColor("green");
 p1.settip(7);
 System.out.println(p1.color +" colour and the tip is "+ p1.tip);
+p1.cat();
 
     }
     
 }
-class pen{
+abstract class Pen{
     String color="blue";
     int tip=1;
     void setColor(String newcolor){
@@ -20,4 +22,11 @@ class pen{
     void settip(int newtip){
         tip= newtip;
     }
+    abstract void cat();
+}
+ class News extends Pen{
+    void cat(){
+        System.out.println("im an extended abstract class");
+    }
+
 }
