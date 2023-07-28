2fd19f09e831ef69503272b1fafa69765262e62f
nivin77789 <nivin77789@gmail.com>
2023-07-28 14:10:32 +0530

    usfdhb

--- recursion.java
+++ recursion.java
@@ -2,16 +2,17 @@ public class recursion {
     public static int rec(int n){
        
        
-if(n==1){
-    return 1;
+if(n==1 | n==0){
+    return n;
 }
-int fn= n+rec(n-1); 
+int feb1=rec(n-1);
+int feb2=rec(n-2);
+int fn= feb1+feb2; 
 
 return fn; 
-   
 }
     public static void main(String args[]){
-        System.out.println(rec(5));
+        System.out.println(rec(50));
 
     }
 }
