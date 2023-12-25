d9d9e34e5ac4448d18b5726ce145c55c72ca461b
nivin77789 <nivin77789@gmail.com>
2023-12-26 04:01:50 +0530

    asf

--- pattern.java
+++ pattern.java
@@ -3,11 +3,34 @@ public class pattern {
    
     public static void main(String args[]){
        
-      pat6(4);
+      pat7(4);
     
 }
 
 
+
+public static void pat7(int row){
+ 
+    for(int i=0; i<=row; i++){
+        for(int j=1; j<=(row-i); j++){
+            System.out.print(" ");
+        }
+        for(int j=1; j<=row; j++){
+            System.out.print("*");
+        }
+        
+    System.out.println();
+    }
+    
+
+
+    /* output
+       1
+       23
+       456
+       78910
+     */
+}
 
 
 
