e6cf863f14ed4fc9521dda5b5388f2054f0ee434
nivin77789 <nivin77789@gmail.com>
2023-12-30 17:57:16 +0530

    fasd

--- pattern.java
+++ pattern.java
@@ -13,21 +13,7 @@ public class pattern {
 
 public static void pat9(int row){
 
-    for(int i=1; i<=row; i++){
-        for(int j=0; j<(row-i); j++){
-            System.out.print("   ");
-        }
-        for(int j=0; j<(2*i)-1; j++){
-            System.out.print(" * ");
-        }
-    System.out.println();
-    }
-     for(int i=row; i>=1; i--){
-        for(int j=(row-i); j>0; j--){
-            System.out.print("   ");
-        }
-        for(int j=(2*i)-1; j>0; j--){
-            System.out.print(" * ");
+
         }
      
     System.out.println();
