e8c5e6eda21f544fd15fb83c62a1396d4bf6a1aa
nivin77789 <nivin77789@gmail.com>
2024-01-14 00:55:30 +0530

    sdafz

--- array.java
+++ array.java
@@ -1,29 +1,25 @@
 public class array {
-    public static void update(int marks[]){
+    public static int update(String marks[],String key){
          for(int i=0; i<marks.length; i++){
-            marks[i]+=1;
+           if(marks[i]==key){
+            return i;
+           }
         }
+        return -1;
 
     }
     public static void main(String args[]){
-        int marks[]={10,20,30};
-        System.out.println();
-        for(int i=0; i<marks.length; i++){
-            System.out.print(marks[i]);
-                    System.out.print(" ");
-        }
-        System.out.println();
-System.out.println();
-System.out.println("after update");
-System.out.println();
- update(marks);
-        for(int i=0; i<marks.length; i++){
-            System.out.print(marks[i]);
-            System.out.print(" ");
-        }
+        String marks[]={"idli","dosa","chutny","biriyani","kabab","masala","tea"};
+      String key="biriyani";
+ int index=update(marks , key);
+       
+ if(index==-1){
+System.out.println("NOT FOUND");
+ } else {
+    System.out.println("item foud at position : " +marks[index]);
+ }
         
-System.out.println();
-System.out.println();
+
     }
     
 }
