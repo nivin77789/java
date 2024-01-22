555951dd62e91fe4bf584999855ecbf4c5689ce0
nivin77789 <nivin77789@gmail.com>
2024-01-22 15:56:06 +0530

    daszg

--- ArraySearch.java
+++ ArraySearch.java
@@ -16,18 +16,29 @@ public class ArraySearch {
           
         }
         public static void group(int arr[]){
-for(int i=0; i<arr.length; i++){
+            for(int i=0; i<arr.length; i++){
     
-    for(int j=i+1; j<arr.length; j++){
-        System.out.print(" ("+arr[i]+","+arr[j]+") ");
-    }
-    System.out.println();
-}
+                for(int j=i+1; j<arr.length; j++){
+                    System.out.print(" ("+arr[i]+","+arr[j]+") ");
+                }
+                    System.out.println();
+                }
         }
+        public static void subarray(int arr[]){
+            for(int i=0; i<arr.length; i++){
     
+                for(int j=i; j<arr.length; j++){
+                    for(int k=i; k<=j; k++){
+                        System.out.print(" "+arr[k]);
+                    }
+                    System.out.print(" | ");
+                }
+                    System.out.println();
+                }
+        }
         public static void main(String[] args) {
             int arr[] = {2,4,6,8,10};
-            group(arr);
+            subarray(arr);
             
     
            
