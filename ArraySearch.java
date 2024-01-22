03ef59ba5bea3f0c93eda5d92c133369293da8c2
nivin77789 <nivin77789@gmail.com>
2024-01-22 15:07:03 +0530

    ds

--- ArraySearch.java
+++ ArraySearch.java
@@ -1,6 +1,6 @@
 public class ArraySearch {
 
-        public static void binarySearch(int arr[]) {
+        public static void reverse(int arr[]) {
             int start = 0, end = arr.length - 1;
     
             while (start < end) {
@@ -10,16 +10,25 @@ public class ArraySearch {
                 start++;
                 end--;
             }
-    
-          
-        }
-    
-        public static void main(String[] args) {
-            int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-            binarySearch(arr);
             for(int i=0; i<=arr.length; i++){
                 System.out.print(arr[i]+" ");
             }
+          
+        }
+        public static void group(int arr[]){
+for(int i=0; i<arr.length; i++){
+    
+    for(int j=i+1; j<arr.length; j++){
+        System.out.print(" ("+arr[i]+","+arr[j]+") ");
+    }
+    System.out.println();
+}
+        }
+    
+        public static void main(String[] args) {
+            int arr[] = {2,4,6,8,10};
+            group(arr);
+            
     
            
         }
