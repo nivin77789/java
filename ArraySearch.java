ff0a57d2227d715f8e57adde4d2fbf23ca414edf
nivin77789 <nivin77789@gmail.com>
2024-01-22 16:04:57 +0530

    dsfa

--- ArraySearch.java
+++ ArraySearch.java
@@ -25,16 +25,26 @@ public class ArraySearch {
                 }
         }
         public static void subarray(int arr[]){
+            int min=arr[0], max=arr[0];
             for(int i=0; i<arr.length; i++){
     
                 for(int j=i; j<arr.length; j++){
+                    int sum=0;
                     for(int k=i; k<=j; k++){
                         System.out.print(" "+arr[k]);
+                        sum+=arr[k];
                     }
-                    System.out.print(" | ");
+if(sum>max){
+    max=sum;
+}
+if(sum<min){
+min=sum;
+}
+                    System.out.print(" sum = "+sum+"  |");
                 }
                     System.out.println();
                 }
+                System.out.println("(  minimum sum = "+min+" maximum sum = "+max+"  )");
         }
         public static void main(String[] args) {
             int arr[] = {2,4,6,8,10};
