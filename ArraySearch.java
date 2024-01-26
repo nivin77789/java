ff1246ca3e21c618ce9becc30e2767eeee30b2f8
nivin77789 <nivin77789@gmail.com>
2024-01-26 16:46:05 +0530

    shg

--- ArraySearch.java
+++ ArraySearch.java
@@ -88,7 +88,7 @@ public class ArraySearch {
             System.out.println("maximun is   "+maxsum);
         }
         public static void main(String[] args) {
-            int arr[] = {-2,-1,-9,-6,1,-9};
+            int arr[] = {4,2,0,6,3,2,5};
             kadans(arr);
             prefix(arr);
     
