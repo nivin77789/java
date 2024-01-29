09685bda641561462abaaf954c7399031901fb6c
nivin77789 <nivin77789@gmail.com>
2024-01-29 10:09:40 +0530

    sgd

--- Hello.java
+++ Hello.java
@@ -2,17 +2,33 @@ import java.util.*;
 public class Hello {
     public static void main(String args[]){
 Scanner sc=new Scanner(System.in);
-        System.out.println("Enter a number");
-        int num=sc.nextInt();
-        for(int i=2; i<=num/2; i++){
-if(num%i==0){
-    System.out.println("Entered number is Not a Prime number");
-    System. exit(0);
-    
-}
+        Sample s=new Sample();
+s.a=sc.nextInt();
+s.b=sc.nextInt();
+        System.out.println("Enter your choice \n1 for Addition\n2 for substraction\n3 for multiplication\n4 for division");
+int choice=sc.nextInt();
+ 
+    switch(choice){
+        case 1:{
+            s.result=s.sum(s.a,s.b);
+            break;
         }
-
-        System.out.println("Entered number is a Prime number");
+        case 2:{
+            s.result=s.sub(s.a,s.b);
+            break;
+        }
+        case 3:{
+            s.result=s.mul(s.a,s.b);
+            break;
+        }
+        case 4:{
+            s.result=s.div(s.a,s.b);
+            break;
+        }
+        default :{
+            System.out.println("Enter valid choice");
+        }
     }
-    
+    System.out.println("The Result is "+s.result);
+    }
 }
--- Sample.java
+++ Sample.java
@@ -1,10 +1,18 @@
 public class Sample {
-    int a,b,c;
-    void sum(int a, int b){
-        c=a+b;
+    int a,b,result;
+    Sample(){
+        System.out.println("Enter two numbers");
     }
-
-    void display(){
-        System.out.println("Sum of a = "+c);
+    int sum(int a, int b){
+        return a+b;
+    }
+    int mul(int a, int b){
+        return a*b;
+    }
+    int sub(int a, int b){
+        return a-b;
+    }
+    int div(int a, int b){
+        return a/b;
     }
 }
